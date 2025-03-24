77960cc766483463854276bebc2a93d4aefe8b6b
MlleJea <jeanne.gruson.jg@gmail.com>
2025-03-24 11:26:09 +0100

    First commit

---
+++ src/main/java/com/eql/ai116/gruson/projet3/Projet_3_BenevOsons/Projet3BenevOsonsApplication.java
@@ -0,0 +1,13 @@
+package com.eql.ai116.gruson.projet3.Projet_3_BenevOsons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet3BenevOsonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet3BenevOsonsApplication.class, args);
+	}
+
+}
